e347bf3ced09548a0fda7e0212cb36a364300220
saqlinezaman <saqlinemoaj@gmail.com>
2025-07-05 15:22:15 +0600

    cash in update and login done

--- js/home.js
+++ js/home.js
@@ -12,4 +12,29 @@ document.getElementById('cash-in-btn').
         const pinNumberInput = document.getElementById('input-pin-number').value;
         console.log(pinNumberInput);
 
+    // validate pin number
+
+        if(pinNumberInput === '1234'){
+            console.log('adding your money in your account');
+
+    // get the current balance 
+        
+        const balance = document.getElementById('balance').innerText;
+            console.log(balance);
+    
+    // sum the cash in money
+            
+            const addMoneyNumber = parseFloat(addMoneyInput);
+            const balanceNumber = parseFloat(balance);
+            const newBalance = addMoneyNumber + balanceNumber ;
+            console.log(newBalance); 
+
+    // update the amount 
+            document.getElementById('balance').innerText = newBalance;
+
+        }
+        else{
+            alert('Cash in faild');
+        }
+
     });
